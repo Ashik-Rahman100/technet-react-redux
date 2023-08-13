d1b1ed6ef7866e632a1eb53dfb533e0fd6679a67
Ashik-Rahman100 <ashik5515@gmail.com>
2023-08-13 15:16:35 +0600

    adding vercel server link

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,6 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   tagTypes: ['comment'],
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://technet-server-seven.vercel.app/',
+  }),
   endpoints: () => ({}),
 });
